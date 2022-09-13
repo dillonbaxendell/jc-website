ce9627db68c92e2cf7d81bb2c3bb0a29667f9ae6
dillonbaxendell <dillon.j.baxendell@gmail.com>
2022-09-13 09:33:46 -0500

    about page created... needs work

--- src/components/AboutPage/AboutPage.jsx
+++ src/components/AboutPage/AboutPage.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './AboutPage.css';
 
 // This is one of our simplest components
 // It doesn't have local state,
@@ -8,8 +9,32 @@ import React from 'react';
 function AboutPage() {
   return (
     <div className="container">
-      <div>
-        <p>This about page is for anyone to read!</p>
+      <div className="screen">
+
+        <div className="grid">
+          <div className="spacer"></div>
+            <div className="grid-col about-grid_col_1">
+              <img className="about-img" src="../Imgs/JCPolygon.png" atl="strongjc"/>
+            </div>
+
+            <div className="grid-col about-grid_col_2" id="typo-content">
+              <h2 className="header" id="about-header">
+                here. me.
+              </h2>
+              <a href="#" class="fa fa-facebook"></a>
+              
+              <div className="socials">
+                
+              </div>
+              <div>
+                <p className="about-paragraph">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incidiunt ut labore et dolore magnum aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </p>
+              </div>
+             
+            </div>
+        </div>
+      
       </div>
     </div>
   );
--- src/components/JCLandingPage/JCLandingPage.jsx
+++ src/components/JCLandingPage/JCLandingPage.jsx
@@ -15,28 +15,26 @@ function JCLandingPage() {
 
   return (
     <div className="container">
-    <div className="screen">
+      <div className="screen">
 
-      <div className="grid">
-        <div className="spacer">
-
-        </div>
-        <div className="center grid-col grid_col_1">
-          <h1>
-            Welcome to my world.
-          </h1>
-          <h2 className="button" onClick={handleContactMe}>
-            contact me
-          </h2>
+        <div className="grid">
+          <div className="spacer"></div>
+          <div className="center grid-col grid_col_1">
+            <h1>
+              Welcome to my world.
+            </h1>
+            <h2 className="button" onClick={handleContactMe}>
+              contact me
+            </h2>
         </div>
 
         
         <div className="grid-col grid_col_2">
-          <img src="../Imgs/jcwithcircle.png" alt="jcwithcircle"/>
+          <img className="landing-img" src="../Imgs/jcwithcircle.png" alt="jcwithcircle"/>
         </div>
 
+        </div>
       </div>
-    </div>
     </div>
   );
 }
